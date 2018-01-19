5bb5371b3408b211a13df075142adc05500811d5
Siddhi Kiran Bajracharya <siddhi.47.skb@gmail.com>
2018-01-19 16:24:37 +0545

    Cursor is controlled by face only. Need to use eyes. Need for window scaling

--- LITE_project.py
+++ LITE_project.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
-
+import pyautogui as m
+m.FAILSAFE = False
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
@@ -16,10 +17,12 @@ while 1:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
-        
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        x = np.int(x)
+        y = np.int(y)
+        print(type(x))
+        print('ex = {} and ey = {}'.format(x, y))
+        m.moveTo(x,y)
+
 
     cv2.imshow('img',img)
     key = cv2.waitKey(1)
