d6514c7ec81e70d804619a477e96ee7653d9379e
Siddhi Kiran Bajracharya <siddhi.47.skb@gmail.com>
2018-01-22 04:41:02 +0545

    added view conversion. Has some issues

--- LITE_project.py
+++ LITE_project.py
@@ -3,8 +3,8 @@ import cv2
 import pyautogui as m
 m.FAILSAFE = False
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
 
 cap = cv2.VideoCapture(0)
 
@@ -19,9 +19,27 @@ while 1:
         roi_color = img[y:y+h, x:x+w]
         x = np.int(x)
         y = np.int(y)
+        #just to check the data type-------------------------
         print(type(x))
-        print('ex = {} and ey = {}'.format(x, y))
-        m.moveTo(x,y)
+        #----------------------------------------------------
+
+
+        # Viewport conversion---------------------------------
+        xvMax, yvMax = m.size()
+        xvMin, yvMin = 0, 0
+        xwMax , xwMin = 240, 80
+        ywMax , ywMin = 205, 70
+        sx = (xvMax-0)//(xwMax-xwMin)
+        sy = (yvMax - 0) // (ywMax - ywMin)
+        xv =  xvMin + (x - xwMin) * sx
+        yv = yvMin + (y - ywMin) * sy
+        #Not working
+
+
+        print('x = {} and y = {}'.format(xv, yv))
+
+
+        m.moveTo(sx*x,sy*y,.1)
 
 
     cv2.imshow('img',img)
